b509b2d3e46349a8da61d9222a2ef3a886e45f91
Pichau <Pichau@DESKTOP-JR0T6JP>
2021-09-25 19:02:43 -0300

    Aula 6 - Trabalhando com Sessões

---
+++ PrograWeb/src/main/java/Trabalhandocomsessoes/ServletLogin.java
@@ -0,0 +1,70 @@
+package Trabalhandocomsessoes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet("/ServletLogin")
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public static final String USUARIO = "Usuário";
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter resposta = response.getWriter();
+
+		resposta.write("<html><body>");
+
+		String nomeUsuario = request.getParameter(USUARIO);
+		String senhaUsuario = request.getParameter("Senha");
+
+		if (autenticar(nomeUsuario,senhaUsuario)) {
+			HttpSession sessao = request.getSession();
+			sessao.setAttribute(USUARIO, nomeUsuario);
+
+			sessao.getAttribute(USUARIO);
+
+			resposta.write("Bem vindo "+ nomeUsuario);
+		} else {
+			resposta.write("Usuário e senha inválidos");
+		}
+		resposta.write("</body></html>");
+	}
+
+	private boolean autenticar(String nomeUsuario, String senhaUsuario) {
+		
+		if (!nomeUsuario.equals("") && !senhaUsuario.equals("") && nomeUsuario != null && senhaUsuario.equals("1234")) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
---
+++ PrograWeb/src/main/webapp/Cadastro.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Dados Pessoais</title>
+</head>
+<body>
+Preencha os seus dados pessoais
+<form action= "ServletTela1Cadastro">
+Nome: <input type="text" name="nome">
+Sobrenome:<input type="text" name="sobrenome">
+Endereço residencial:
+Rua: <input type="text" name="rua">
+Complemento: <input type="text" name="complemento"><b>
+Cidade: <input type="text" name="cidade">
+CEP: <input type="text" name="cep">
+Estado: <input type="text" name="cidade"><b>
+</form>
+</body>
+</html>
